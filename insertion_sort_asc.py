20c79a2b9ab7f3fd0c4d55b61b0b603f73434e17
caocchinh <chinhcaocu@gmail.com>
2026-03-04 15:19:56 +0700

    feat: Implement `deQueue` and refine `countItem` in `linear_queue`, and add explanatory comments to `insertion_sort`.

--- insertion_sort_asc.py
+++ insertion_sort_asc.py
@@ -3,16 +3,27 @@
 # Function to sort array using insertion sort
 def insertionSort(arr):
     for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
+        key = arr[i] # 5
+        j = i - 1 # 2
 
         # Move elements of arr[0..i-1], that are
         # greater than key, to one position ahead
         # of their current position
         while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
+            # 3 (13) = 13
+            # 2 (13) = 12
+            # 1 (12) = 11
             j -= 1
+            # j = 1
+            # = 0
+            # = -1
+            # [11, 12, 13, 13, 6]
+            # [11, 12, 12, 13, 6]
+            # [11, 11, 12, 13, 6]
         arr[j + 1] = key
+        # arr[0] = 5
+        # [5, 11, 12, 13, 6]
 
 # A utility function to print array of size n
 def printArray(arr):
@@ -22,6 +33,6 @@ def printArray(arr):
 
 # Driver method
 if __name__ == "__main__":
-    arr = [12, 11, 13, 5, 6]
+    arr = [11, 12, 13, 5, 6]
     insertionSort(arr)
     printArray(arr)
--- linear_queue.py
+++ linear_queue.py
@@ -1,4 +1,4 @@
-queue = [-1] * 5
+queue = [None] * 5
 headIndex = -1
 freeIndex = 0
 
@@ -12,14 +12,32 @@ def enQueue(value):
     freeIndex += 1
     return True
 
-def countItem(start):
-    global freeIndex, queue
-    if (start < freeIndex):
-        return queue[start] + countItem(start - 1)
-    return 0
+def deQueue():
+    global headIndex, freeIndex, queue
+    if (headIndex == -1):
+        return False
+    queue[headIndex] = None
+    headIndex += 1
+    if (headIndex == freeIndex):
+        headIndex = -1
+        freeIndex = 0
+    return True
+
+def countItem():
+    global headIndex, freeIndex
+    if headIndex == -1:
+        return 0
+    return freeIndex - headIndex
+
+
 
 enQueue(2)
 enQueue(3)
 enQueue(4)
+enQueue(4)
+enQueue(4)
+enQueue(4)
+
+
 print(queue)
-print(countItem(headIndex))
+print(countItem())
